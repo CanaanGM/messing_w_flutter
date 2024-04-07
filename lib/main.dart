b78eda9b739cea1a5ae361fc3a58382a791a67a8
CanaanGM <Reakak1@gmail.com>
2024-04-07 18:18:52 +0300

    const is const but is const a const?

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            ImageViewBuilderHorizontal(),
+            const ImageViewBuilderHorizontal(),
             const ImageViewBuilderVertical(),
           ],
         ),
